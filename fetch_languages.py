f07d5bd2b786df1728890e350b899a392e33c926
Pavel Navitski <navpav2002@mail.ru>
2024-04-06 00:48:20 +0200

    Update fetch_languages.py

--- fetch_languages.py
+++ fetch_languages.py
@@ -1,6 +1,11 @@
 import os
 import requests
+import matplotlib.pyplot as plt
 
+import matplotlib
+matplotlib.use('Agg')
+
+# Token und Header für die GitHub API
 token = os.getenv('STATS_TOKEN')
 headers = {
     'Authorization': f'token {token}',
@@ -13,17 +18,21 @@ all_languages = {}
 
 for repo in repos:
     response = requests.get(f'https://api.github.com/repos/{repo}/languages', headers=headers)
-    repo_languages = response.json()
-    for language, size in repo_languages.items():
-        if language in all_languages:
-            all_languages[language] += size
-        else:
-            all_languages[language] = size
+    if response.ok:
+        repo_languages = response.json()
+        for language, size in repo_languages.items():
+            all_languages[language] = all_languages.get(language, 0) + size
+    else:
+        print(f"Fehler beim Abrufen der Daten für Repo {repo}")
 
-languages_str = '\n'.join([f'{lang}: {size}' for lang, size in all_languages.items()])
+plt.figure(figsize=(10, 8))
+plt.bar(all_languages.keys(), all_languages.values(), color='blue')
+plt.title('Programming Languages Usage')
+plt.xlabel('Languages')
+plt.ylabel('Bytes of Code')
+plt.style.use('dark_background')
+plt.savefig('languages_usage_chart.png', facecolor='black')
 
-with open('LANGUAGES.md', 'w') as file:
-    file.write(languages_str)
 print(response.json())
 print(f'Schreibe Sprachstatistik in LANGUAGES.md: \n{languages_str}')
 print(f"Speichern der Daten in {os.path.abspath('LANGUAGES.md')}")
