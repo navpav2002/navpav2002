f38d8b9abb016cb100894b6551c998f714d420c2
Pavel Navitski <navpav2002@mail.ru>
2024-04-06 00:56:34 +0200

    Update fetch_languages.py

--- fetch_languages.py
+++ fetch_languages.py
@@ -2,6 +2,7 @@ import os
 import requests
 import matplotlib.pyplot as plt
 
+# Sicherstellen, dass matplotlib keinen Display benötigt
 import matplotlib
 matplotlib.use('Agg')
 
@@ -12,8 +13,10 @@ headers = {
     'Accept': 'application/vnd.github.v3+json',
 }
 
+# Liste Ihrer Repositories
 repos = ['navpav2002/react-int', 'navpav2002/Assembly', 'navpav2002/Docker_INT', 'navpav2002/JS-AA1']
 
+# Sammeln der Sprachdaten
 all_languages = {}
 
 for repo in repos:
@@ -25,14 +28,25 @@ for repo in repos:
     else:
         print(f"Fehler beim Abrufen der Daten für Repo {repo}")
 
+# Erstellen des Balkendiagramms
+languages = list(all_languages.keys())
+sizes = list(all_languages.values())
+
+# Dunklen Hintergrund für das Diagramm einstellen
 plt.figure(figsize=(10, 8))
-plt.bar(all_languages.keys(), all_languages.values(), color='blue')
+plt.bar(languages, sizes, color='blue')
 plt.title('Programming Languages Usage')
 plt.xlabel('Languages')
 plt.ylabel('Bytes of Code')
 plt.style.use('dark_background')
-plt.savefig('languages_usage_chart.png', facecolor='black')
 
-print(response.json())
-print(f'Schreibe Sprachstatistik in LANGUAGES.md: \n{languages_str}')
-print(f"Speichern der Daten in {os.path.abspath('LANGUAGES.md')}")
+# Speichern des Diagramms als Bild
+diagram_path = 'languages_usage_chart.png'
+plt.savefig(diagram_path, facecolor='black')
+
+# Sicherstellen, dass das Bild korrekt gespeichert wurde
+if os.path.exists(diagram_path):
+    print(f"Balkendiagramm wurde als {diagram_path} gespeichert.")
+else:
+    print("Fehler beim Speichern des Balkendiagramms.")
+
