5a06391dc0cce2bcd50d02a88ddb06411472cb25
Pavel Navitski <navpav2002@mail.ru>
2024-04-06 01:20:59 +0200

    Update fetch_languages.py

--- fetch_languages.py
+++ fetch_languages.py
@@ -2,7 +2,7 @@ import os
 import requests
 import matplotlib.pyplot as plt
 
-# Stellen Sie sicher, dass matplotlib keinen Display benötigt
+# Sicherstellen, dass matplotlib keinen Display benötigt
 import matplotlib
 matplotlib.use('Agg')
 
@@ -40,30 +40,29 @@ sizes = list(all_languages.values())
 # Festlegen einer Liste von Farben für die Balken
 colors = ['#FF5733', '#33FF57', '#3357FF', '#F033FF', '#FF33F0', '#33FFF3']
 
+# Diagrammstil für dunklen Hintergrund verwenden
+plt.style.use('dark_background')
+
 plt.figure(figsize=(10, 8))
 bars = plt.bar(languages, sizes, color=colors)
 
-# Hintergrund und Schriftfarben festlegen
-# plt.gcf().set_facecolor('black')
-# plt.gca().set_facecolor('black')
-plt.xticks(color='white')
+# Beschriftungen und Titel hinzufügen
+plt.xticks(ticks=range(len(languages)), labels=languages, color='white')
 plt.yticks(color='white')
 plt.xlabel('Languages', color='white')
 plt.ylabel('Bytes of Code', color='white')
 plt.title('Programming Languages Usage', color='white')
 
-# Achsenfarben ändern
-for spine in plt.gca().spines.values():
-    spine.set_color('white')
+# Achsenfarben bereits durch den Stil gesetzt
 
 # Beschriftungen auf den Balken hinzufügen
-for bar, language in zip(bars, languages):
+for bar in bars:
     yval = bar.get_height()
     plt.text(bar.get_x() + bar.get_width()/2.0, yval, f'{int(yval)}', va='bottom', ha='center', color='white')
 
 # Speichern des Diagramms als Bild mit einem dunklen Hintergrund
 diagram_path = 'languages_usage_chart.png'
-plt.savefig(diagram_path, facecolor='black')
+plt.savefig(diagram_path, facecolor='black')  # Hintergrundfarbe des gespeicherten Bildes
 
 # Sicherstellen, dass das Bild korrekt gespeichert wurde
 if os.path.exists(diagram_path):
